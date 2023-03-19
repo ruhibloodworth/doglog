a341c3d3fc7501255fb6753a1259d48624bc7392
Ruhi Bloodworth <ruhi.bloodworth@gmail.com>
2023-03-18 23:06:49 -0500

    scaffold backend

---
+++ backend/src/doglog_backend/tests/test_users.py
@@ -0,0 +1,78 @@
+from fastapi.testclient import TestClient
+from pathlib import Path
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from ..database import Base
+from ..main import app, get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+Path("./test.db").unlink(missing_ok=True)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+client = TestClient(app)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_db(request):
+    response = client.post(
+        "/users/",
+        json={"email": "adam@example.com", "password": "password"},
+    )
+
+
+def test_create_user():
+    response = client.post(
+        "/users/",
+        json={"email": "deadpool@example.com", "password": "chimichangas4life"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["email"] == "deadpool@example.com"
+    assert "id" in data
+    user_id = data["id"]
+
+    response = client.get(f"/users/{user_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["email"] == "deadpool@example.com"
+    assert data["id"] == user_id
+
+
+def test_get_users():
+    response = client.get(
+        "/users/"
+    )
+    assert response.status_code == 200, response.text
+    data = [u for u in response.json() if u["email"] == "adam@example.com"]
+    assert len(data) == 1
